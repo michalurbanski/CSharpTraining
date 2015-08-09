c42922ed53806f2e323bcec525528bd68ee304ae
michal_urbanski <michal_urbanski@windowslive.com>
2015-08-09 20:44:43 +0200

    Adds IEnumerable implementation example and yield return usage in custom enumerator

---
+++ FirstProject/FirstProject/PeopleCollection.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    public class PeopleCollection : IEnumerable<Person>
+    {
+        private List<Person> _peopleCollection = new List<Person>();
+
+        public void AddPerson(Person person)
+        {
+            _peopleCollection.Add(person);
+        }
+
+        /// <summary>
+        /// Custom enumerator implementation - gets under age people
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<Person> GetUnderAgePeople()
+        {
+            foreach(Person person in _peopleCollection)
+            {
+                if (person.Age <= 21)
+                    yield return person; 
+            }
+        }
+
+        #region IEnumerable
+
+        public IEnumerator<Person> GetEnumerator()
+        {
+            return _peopleCollection.GetEnumerator(); 
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        #endregion
+    }
+}
--- FirstProject/FirstProject/Program.cs
+++ FirstProject/FirstProject/Program.cs
@@ -6,10 +6,47 @@ using System.Threading.Tasks;
 
 namespace FirstProject
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            PeopleCollectionEnumerationExample();
+
+            Console.ReadLine(); 
+        }
+
+        public static void PeopleCollectionEnumerationExample()
+        {
+            PeopleCollection people = new PeopleCollection();
+            people.AddPerson(new Person()
+            {
+                Name = "Mike", 
+                Age = 22
+            });
+
+            people.AddPerson(new Person()
+            {
+                Name = "Kate",
+                Age = 17
+            });
+
+            people.AddPerson(new Person()
+            {
+                Name = "John",
+                Age = 20
+            });
+
+            Console.WriteLine("All people");
+            foreach (Person person in people)
+            {
+                Console.WriteLine(person);
+            }
+
+            Console.WriteLine("\r\nUnder age people");
+            foreach (Person person in people.GetUnderAgePeople())
+            {
+                Console.WriteLine(person);
+            }
         }
     }
 }
