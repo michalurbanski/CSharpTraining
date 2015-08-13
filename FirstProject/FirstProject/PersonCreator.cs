54c922b4599a5ca7fcc33698dcf367dd3b4eec2d
michal_urbanski <michal_urbanski@windowslive.com>
2015-08-13 20:23:34 +0200

    Adds object creation using type full name

---
+++ FirstProject/FirstProject/PersonCreator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    public class ObjectCreator
+    {
+        public T CreateFromTypeUsingDefaultConstructor<T>(string typeName) where T : class, new()
+        {
+            T element = null; 
+
+            Type t = Type.GetType(typeName, false);
+            if (t != null)
+            {
+                element = (T)Activator.CreateInstance(t);
+            }
+
+            return element; 
+        }
+
+    }
+}
---
+++ FirstProject/FirstProjectUnitTests/PersonCreatorTests.cs
@@ -0,0 +1,29 @@
+﻿using FirstProject;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProjectUnitTests
+{
+    [TestClass]
+    public class PersonCreatorTests
+    {
+        [TestMethod]
+        public void Test_CreatePersonFromTypeName_ShouldCreateProperObject()
+        {
+            // Arrange
+            ObjectCreator creator = new ObjectCreator();
+            string expectedType = "FirstProject.Person";
+
+            // Act 
+            Person person = creator.CreateFromTypeUsingDefaultConstructor<Person>(expectedType);
+
+            // Assert
+            Assert.IsNotNull(person);
+            Assert.AreEqual(expectedType, person.GetType().FullName);
+        }
+    }
+}
